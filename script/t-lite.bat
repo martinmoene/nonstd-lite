c4e6547e040ba939650000392266d08c6d91d409
Martin Moene <martin.moene@gmail.com>
2021-07-05 16:48:00 +0200

    Add scripts to compile tests of *-lite projects

---
+++ script/t-lite.bat
@@ -0,0 +1,23 @@
+@echo off & setlocal enableextensions enabledelayedexpansion
+::
+:: tg-lite-msys.bat - compile & run tests (GNUC).
+::
+
+set SCRIPT_DIR=%~dp0
+
+::active-lite
+::endian-lite
+::invoke-lite
+::nonstd-lite
+::typetag-lite
+set FOLDERS=any-lite atomic-lite bit-lite boolean-lite byte-lite expected-lite jthread-lite observer-ptr-lite optional-fun-lite optional-lite ring-span-lite scope-lite span-lite status-value-lite string-lite string-view-lite type-lite value-ptr-lite variant-lite
+
+for %%d in (%FOLDERS%) do (
+    echo.
+    echo ==============
+    echo %%d:
+    cd %SCRIPT_DIR%../../%%d/test
+    call t.bat
+)
+
+endlocal & goto :EOF
